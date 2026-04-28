10059ba09d8129e8860cf845b2aed3035f3a7386
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-04-28 16:52:04 -0400

    add queue.go to pubsub package

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connstring := "amqp://guest:guest@localhost:5672"
+	connection, err := amqp.Dial(connstring)
+	if err != nil {
+		fmt.Println(
+			fmt.Errorf("Error establishing RabbitMQ connection: %w", err),
+		)
+	}
+	defer connection.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println(
+			fmt.Errorf("Error getting username: %w", err),
+		)
+	}
+
 }
---
+++ internal/pubsub/queue.go
@@ -0,0 +1,45 @@
+package pubsub
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type SimpleQueueType string
+
+const (
+	Durable   SimpleQueueType = "durable"
+	Transient SimpleQueueType = "transient"
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	queue, err := channel.QueueDeclare(
+		queueName,
+		queueType == "durable",
+		queueType == "transient",
+		queueType == "transient",
+		false, nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = channel.QueueBind(
+		queueName,
+		key,
+		exchange,
+		false, nil,
+	)
+
+	return channel, queue, nil
+}
